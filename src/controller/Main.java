ecbc3034e5c2fd16ac4f41f786fbe4300ccc62ed
sayali1098 <sauamritkar@gmail.com>
2020-07-16 21:25:18 +0530

    FSD0620B102

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,28 @@
+package controller;
 
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main
+{
+	public static void main(String args[])throws Exception
+	{
+		SkillDAO skilldao = new SkillDAO();
+		
+		TreeMap<Integer,Skill> map = new TreeMap<Integer,Skill>();
+		map=skilldao.skillCount();
+		System.out.println("Skill"+"\t"+"Players");
+		
+		for(Entry<Integer,Skill> e: map.entrySet())
+		{
+			int  count=e.getKey();
+			Skill skill=e.getValue();
+			System.out.println(skill.getSkillName() + "\t" + count);
+		}	
+	}
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,57 @@
+package dao;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.TreeMap;
+import java.util.Map.Entry;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	public TreeMap<Integer,Skill> skillCount() throws Exception 
+	{
+		Connection con=ConnectionManager.getConnection();
+		Statement st =  con.createStatement();
+		TreeMap<Integer,Skill> map=new TreeMap<Integer,Skill>();	
+		int raiding = 0;
+		int defending = 0;
+		int jumping = 0;
+		
+		ResultSet rs = st.executeQuery("SELECT * FROM PLAYER");		
+		while(rs.next())
+		{
+			if(rs.getInt(4)==1)
+			{
+				jumping++;
+			}
+			else if(rs.getInt(4)==2)
+			{
+				defending++;
+			}
+			else if(rs.getInt(4)==3)
+			{
+				raiding++;
+			}
+		}
+		Integer i1=new Integer(raiding);
+		Integer i2=new Integer(defending);
+		Integer i3=new Integer(jumping);
+		
+		Skill s1=new Skill((long)1,"raider");
+		Skill s2=new Skill((long)2,"defender");
+		Skill s3=new Skill((long)3,"jumper");
+		
+		map.put(i1,s1);
+		map.put(i2,s2);
+		map.put(i3,s3);
+		con.close();	
+		return map;
+	
+	}
+	
+}
